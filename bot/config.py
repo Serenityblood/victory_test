8d683298c7662cb63c0b4f8c5cd9e99b56ded908
serenityblood <miracle_5@bk.ru>
2025-07-19 00:38:31 +0500

    clear

--- bot/config.py
+++ bot/config.py
@@ -8,7 +8,7 @@ ALLOWED_TO_MAILING_CONSTRUCTOR_ROLES = (
     "moderator",
 )
 ALLOWED_MEDIA_TYPES = ("animation", "photo", "video")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7768032008:AAEQm0ynbpOl11Mj6j625v0F-OKOqlZBoZs")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 API_URL = os.getenv("API_URL", "http://localhost:8000/api/v1/")
 SECRET_KEY = os.getenv("SECRET_KEY", "d3cb099dfcbe5c1f2d0514417928df9a")
 REDIS_URL = os.getenv("REDIS_URL_FOR_BOT", "redis://localhost:6379/0")
